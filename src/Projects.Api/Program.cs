4ad1de847e18e42fbbb91047321c7184f4c2c615
Дар'я Завтрак <zavdariag@gmail.com>
2026-07-27 10:31:26 +0300

    fix: expose Swagger UI on production for Projects.Api

--- src/Projects.Api/Program.cs
+++ src/Projects.Api/Program.cs
@@ -40,12 +40,11 @@ using (var scope = app.Services.CreateScope())
 }
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.RoutePrefix = "docs/swagger";
+});
 
 app.UseHttpsRedirection();
 
